1646af7b62781c7ed280322aa7a2bfdbda0e96b0
PongpornFLK <pongporn.yt2910@gmail.com>
2025-11-24 17:24:34 +0700

    First Commit

---
+++ backend/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Pongporn Test FastAPI"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)

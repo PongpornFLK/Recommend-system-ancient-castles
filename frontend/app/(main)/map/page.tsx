59a2d514f0e4e5a4903752ab55077cdf3279b0c7
PongpornFLK <pongporn.yt2910@gmail.com>
2026-02-28 22:03:08 +0700

    feat: implement map component to display user current location with geocoding

--- frontend/app/(main)/map/page.tsx
+++ frontend/app/(main)/map/page.tsx
@@ -1,13 +1,24 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { useState } from "react";
 
-const MyMap = dynamic(() => import("@/app/components/map_current"), { ssr: false });
+const MapCurrent = dynamic(() => import("@/app/components/map/map_current"), {
+  ssr: false,
+});
 
 export default function Map() {
+  const [getNamePlace, setNamePlace] = useState("");
+
   return (
-    <div>
-        <MyMap />
+    <div className="bg-white p-5 place-items-center rounded-xl">
+      <div className="flex flex-col my-5 gap-5 items-center">
+        <div className="text-3xl font-bold">Map user current location</div>
+        <div className="bg-tone-lightgray py-3 px-10 rounded-xl">{getNamePlace}</div>
+      </div>
+      <div className="w-full">
+        <MapCurrent namePlace={setNamePlace} />
+      </div>
     </div>
-  )
+  );
 }

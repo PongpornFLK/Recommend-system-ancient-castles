99b970536bb320c3b11ae164b06ee53c87daa4b1
PongpornFLK <pongporn.yt2910@gmail.com>
2026-03-02 23:41:53 +0700

    feat: add ViewMap and useRoutedata components for route visualization

---
+++ frontend/app/service/viewroute/useRoutedata.ts
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface RouteProps {
+  current: { lat: number; lng: number };
+  nearbyplace: { placeName: string; latitude: number; longitude: number }[];
+  destination: {
+    castle_name: string;
+    location: { latitude: number; longitude: number };
+  } | null;
+  currentname: string;
+}
+
+export default function useRoutedata() {
+  const [routeData, setRouteData] = useState<RouteProps | null>(null);
+
+  useEffect(() => {
+    const fetchRoute = () => {
+      const myRoute = localStorage.getItem("my_route");
+      if (myRoute) {
+        const route = JSON.parse(myRoute);
+        setRouteData(route);
+        // console.log("Route Data :", route);
+
+      } else {
+        console.log("No route data found in localStorage");
+      }
+    };
+    fetchRoute();
+  }, []);
+  return { routeData };
+}

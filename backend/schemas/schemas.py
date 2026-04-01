5a9f7465c132fe59fd63d7631aeeeda2649e28a5
Nattha45 <139251222+Nattha45@users.noreply.github.com>
2026-04-01 19:46:19 +0700

    class NearbyPlaceCreate
    castle_id: int
        place_name: str
        nearby_detail: str
        latitude: float
        longitude: float

--- backend/schemas/schemas.py
+++ backend/schemas/schemas.py
@@ -17,6 +17,13 @@ class UploadDocumentVectorResponse(BaseModel):
     document_name: str
     chunks_inserted: int
 
+class NearbyPlaceCreate(BaseModel):
+    castle_id: int
+    place_name: str
+    nearby_detail: str
+    latitude: float
+    longitude: float
+
 class CastleFullCreate(BaseModel):
     castle_name: str
     castle_description: Optional[str] = None
